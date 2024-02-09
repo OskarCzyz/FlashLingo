deee6273729225f9ba1fa28cf6a1a0536bca695d
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2024-02-09 18:06:37 +0100

    deleted not sure button, basic animation

--- SQLiteCRUD/app/src/main/java/com/example/sqlitecrud/FlashCardActivity.java
+++ SQLiteCRUD/app/src/main/java/com/example/sqlitecrud/FlashCardActivity.java
@@ -2,15 +2,51 @@ package com.example.sqlitecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.TextView;
 
 public class FlashCardActivity extends AppCompatActivity {
 
+    private TextView tvFlashCardtext;
+    private String EnglishText = "Cat";
+
+    private String PolishText = "Kot";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_card);
 
+        tvFlashCardtext = findViewById(R.id.tvFlashCardtext);
 
+        tvFlashCardtext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ObjectAnimator oa1 = ObjectAnimator.ofFloat(tvFlashCardtext, "scaleX", 1f, 0f);
+                final ObjectAnimator oa2 = ObjectAnimator.ofFloat(tvFlashCardtext, "scaleX", 0f, 1f);
+                oa1.setInterpolator(new DecelerateInterpolator());
+                oa2.setInterpolator(new AccelerateDecelerateInterpolator());
+                oa1.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        if (tvFlashCardtext.getText() == EnglishText) {
+                            tvFlashCardtext.setText(PolishText);
+                        } else {
+                            tvFlashCardtext.setText(EnglishText);
+                        }
+
+                        oa2.start();
+                    }
+                });
+                oa1.start();
+            }
+        });
     }
+
 }
